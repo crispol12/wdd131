6d0a3449d0f320651fb3a85e04fe202e59330672
crispol12 <cristian.paul14@gmail.com>
2024-10-20 00:49:48 -0500

    change

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,4 @@
-// Calcular el factor de enfriamiento por viento
+
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
         let windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
@@ -8,16 +8,16 @@ function calculateWindChill(temperature, windSpeed) {
     }
 }
 
-// Obtener la última fecha de modificación del documento
-document.addEventListener('DOMContentLoaded', () => {
-    const temp = 20; // Temperatura estática
-    const windSpeed = 10; // Velocidad del viento estática
 
-    // Calcular y mostrar el windchill
+document.addEventListener('DOMContentLoaded', () => {
+    const temp = 20; 
+    const windSpeed = 10; 
+
+
     const windChillElement = document.getElementById('windchill');
     windChillElement.textContent = calculateWindChill(temp, windSpeed);
 
-    // Mostrar la última fecha de modificación del documento
+    
     const lastModifiedElement = document.getElementById('last-modified');
     lastModifiedElement.textContent = document.lastModified;
 });
